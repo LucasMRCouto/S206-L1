47c310e0a32722baf76e6d713bec6121eb654697
Lucas Couto <lucascouto04@hotmail.com>
2023-11-20 16:39:06 -0300

    Aula 2 - api

---
+++ aula_karate/src/test/java/aula_inatel/pokemon/pokemonRunner.java
@@ -0,0 +1,12 @@
+package aula_inatel.pokemon;
+
+import com.intuit.karate.junit5.Karate;
+
+class pokemonRunner {
+    
+    @Karate.Test
+    Karate testPokemon() {
+        return Karate.run("pokemon").relativeTo(getClass());
+    }    
+
+}
